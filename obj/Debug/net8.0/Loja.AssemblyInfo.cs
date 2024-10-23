ac2a07277dfc92fd0ac4e059a2886ce7d8dea1f1
Janerio Gustavo <janerio32@hotmail.com>
2024-10-23 17:14:40 -0300

    just small adjustments

--- obj/Debug/net8.0/Loja.AssemblyInfo.cs
+++ obj/Debug/net8.0/Loja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e1b94c2b9ce7daf165fbd5e2ed37acab69d3d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0623c881262a094b6ee5607faea9207c952942f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
